e9b31c7865ec4a928da3991f065cb8044ed133a0
Raiyan Firaz <82383890+raiyan1437@users.noreply.github.com>
2022-07-21 12:35:23 +0530

    Update pred.py

--- pred.py
+++ pred.py
@@ -23,8 +23,8 @@ def app():
     else:
         if st.button("Predict"):
             try:
-		        video_file = open('myvideo.mp4', 'rb')
-		        video_bytes = video_file.read()
+		video_file = open('myvideo.mp4', 'rb')
+	        video_bytes = video_file.read()
                 st.video(video_bytes)
 
                 #vid = Video.open(file)            
