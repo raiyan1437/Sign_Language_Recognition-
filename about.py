9bb970b3997cae7c02b58f6daa678fe2284e26f8
Raiyan Firaz <82383890+raiyan1437@users.noreply.github.com>
2022-07-22 23:40:09 +0530

    Update about.py

--- about.py
+++ about.py
@@ -13,8 +13,8 @@ def app():
       st.markdown('''### Email:
       raiyan1437@gmail.com''')
 
-      st.markdown('''### GitHub: [Raiyan Firaz] (https://github.com/raiyan1437)''')
+      st.markdown('''# GitHub: [Raiyan Firaz] (https://github.com/raiyan1437)''')
 
-      st.markdown('''### Linkedin: [Raiyan Firaz] (https://www.linkedin.com/in/raiyan-firaz-490b38129)''')
+      st.markdown('''# Linkedin: [Raiyan Firaz] (https://www.linkedin.com/in/raiyan-firaz-490b38129)''')
       
-      st.markdown('''### Repository Link:[Github Repo] (https://github.com/raiyan1437/Sign_Language_Recognition-/)''')
+      st.markdown('''# Repository Link:[Github Repo] (https://github.com/raiyan1437/Sign_Language_Recognition-/)''')
