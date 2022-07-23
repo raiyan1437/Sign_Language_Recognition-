aa50fc91de843890a3515849d370e4676ffeb779
Raiyan Firaz <82383890+raiyan1437@users.noreply.github.com>
2022-07-23 09:50:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,5 +3,5 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('convlstm_model___Date_Time_2022_06_24__08_08_47___Loss_0.8155478835105896___Accuracy_0.6747967600822449 (1).h5')
+  model = tf.keras.models.load_model('convlstm_model___Date_Time_2022_07_23__02_40_22___Loss_0.18381743133068085___Accuracy_0.8834951519966125.h5')
   return model
